da8b77b617b5f82fe128566d79469f819276cf14
Ricky Mormor <rickymormor1@gmail.com>
2022-03-10 15:09:13 +0000

    Add teacher class

---
+++ teacher.rb
@@ -0,0 +1,11 @@
+require './person.rb'
+
+class Teacher < Person
+  def initialize (specialization = "unknown" name = "unknown", age, parent_permission = "true")
+    super(name, age, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+end
