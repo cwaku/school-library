7598ec1f7731b86ec05cdc1784def1bce2c977e6
Ricky Mormor <rickymormor1@gmail.com>
2022-03-10 15:08:38 +0000

    Add person class

---
+++ person.rb
@@ -0,0 +1,25 @@
+class Person
+  def initialize(name = "unknown", age, parent_permission = "true")
+    @name = name
+    @age = age
+    @id = rand(1..1000)
+    @parent_permission = parent_permission
+  end
+
+  attr_accessor :name, :age
+  attr_reader :id
+
+  is_of_age?
+
+  def can_use_services?
+    return true if @parent_permission == "true" || @age >= 18
+    false
+  end
+
+  private
+
+  def is_of_age?
+    return true if @age >= 18
+    false
+  end
+end
