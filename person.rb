87eced3ab93747cf0493701236496007b4d61f5b
Ricky Mormor <rickymormor1@gmail.com>
2022-03-10 18:32:47 +0000

    Check for maximum characters

--- person.rb
+++ person.rb
@@ -27,8 +27,11 @@ class Person < Namable
   end
 
   def correct_name
-    @name
-  end
+    if @namable.correct_name.length > 10
+      @nameable.correct_name[0..9]
+    else
+      @nameable.correct_name
+    end
 
   private
 
